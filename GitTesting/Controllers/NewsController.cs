e8e56142c7fe10e1e3654a5b2ca7f8f78ebb551d
Tomasz Czujewicz <tomasz.czujewicz@geminisoftnet.com>
2015-06-12 15:53:52 +0200

    news 0.1

---
+++ GitTesting/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using GitTesting.ViewModels.News;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitTesting.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+        public ActionResult RecentList()
+        {
+            var model = new List<NewsViewModel>(); 
+            var r =new Random(); 
+            var dtNow=DateTime.Now; 
+            for(int i=0; i<10;i++)
+            {
+                var n = new NewsViewModel { Title="news-"+i, Published=true, CreateDate=dtNow.AddHours(r.Next(1,24)), Content="hello world"}; 
+                model.Add(n);
+            }
+
+            return View(model.OrderByDescending(x=>x.CreateDate).ToList());
+        }
+	}
+}
---
+++ GitTesting/ViewModels/News/NewsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitTesting.ViewModels.News
+{
+    public class NewsViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool Published { get; set; }
+        public DateTime CreateDate { get; set; } 
+    }
+}
